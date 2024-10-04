b6a85e7335a6081fdbbfe415f208c8b86c110468
Abhinav <abhinavchauhan559@gmail.com>
2024-10-04 17:48:49 +0530

    Video 4 completed

---
+++ basic_1/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>Counter value: {count} </h2>
+
+      <div>
+        <button
+          style={{ marginRight: "10px" }}
+          onClick={decrement}
+          disabled={count === 0}
+        >
+          -
+        </button>
+        <button onClick={increment} disabled={count === 20}>+</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
